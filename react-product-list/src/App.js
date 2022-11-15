629ebff36c95f79db097173acddeda915b4d2925
Salo <salomonco@gmail.com>
2022-11-15 18:42:09 -0500

    challeng 9

--- react-product-list/src/App.js
+++ react-product-list/src/App.js
@@ -10,18 +10,34 @@ import data, {allCategories, uniqueCategories, categoriesAndCount, catsAndCounts
 function App() {
   return (
     <div className="App">
-<h1>Productify</h1>
-<p>Products Count: {data.length}</p>
-<p>Category Count: {uniqueCategories.length}</p>
-{catsAndCounts.map(obj => {
-  return(
-    <button>{obj.name}
-    <span> {obj.count} </span>
-    </button>
-  )
+      <h1>Productify</h1>
+      <p>Products Count: {data.length}</p>
+      <p>Category Count: {uniqueCategories.length}</p>
+      <div>
+        {catsAndCounts.map((obj) => {
+          return (
+            <button>
+              {obj.name}
+              <span> {obj.count} </span>
+            </button>
+          );
+        })}
+      </div>
+
+      <div>
+        {data.map((obj) => {
+          return(
+            <div>
+              <h2>{obj.name}</h2>
+              <p>{obj.description}</p>
+              <p>{obj.price}</p>
+              <small>Category: {obj.category} Rating: {obj.rating}</small>
+            </div>
+          )
+        })}
 
 
-})}
+      </div>
     </div>
   );
 }
